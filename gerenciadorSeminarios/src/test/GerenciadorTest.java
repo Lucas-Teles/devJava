f9cebfb15b190e2286956f96357678bd46c7e34b
Lucas Teles <lucasouzateles@gmail.com>
2024-05-01 18:43:18 -0300

    feat: Adicionando primeiro exercicio.

---
+++ gerenciadorSeminarios/src/test/GerenciadorTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import dominio.Aluno;
+import dominio.Local;
+import dominio.Professor;
+import dominio.Seminario;
+
+public class GerenciadorTest {
+    public static void main(String[] args) {
+        Local local = new Local("Rua das laranjeiras");
+        Aluno aluno = new Aluno("Lucas", 27);
+        Professor professor = new Professor("Marcos", "Filosofia");
+        Aluno[] alunosParaSeminario = {aluno};
+        Seminario seminario = new Seminario("O que é filosofia?", alunosParaSeminario, local);
+        Seminario[] seminariosDisponiveis = {seminario};
+        professor.setSeminarios(seminariosDisponiveis);
+
+        professor.imprime();
+    }
+}
