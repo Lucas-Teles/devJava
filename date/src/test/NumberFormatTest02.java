a2366f921c6a4efa942c41bff96a9eaa5db7f5fb
Lucas Teles <lucasouzateles@gmail.com>
2024-05-10 16:29:59 -0300

    feat: adicionando aprendizado de datas

---
+++ date/src/test/NumberFormatTest02.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest02 {
+    public static void main(String[] args) {
+        Locale localePT = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+        NumberFormat[] nfa = new NumberFormat[4];
+
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeIT);
+        nfa[3] = NumberFormat.getCurrencyInstance(localePT);
+
+        double valor = 1_000.2130;
+
+        for (NumberFormat numberFormat : nfa) {
+            numberFormat.setMaximumFractionDigits(2);
+            System.out.println(numberFormat.format(valor));
+        }
+    }
+}
